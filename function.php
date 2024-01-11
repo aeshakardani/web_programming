2c740194c3cf2ae84d2facb6aa6d72f737ac53b0
aeshakardani <155123802+aeshakardani@users.noreply.github.com>
2024-01-11 08:44:15 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,36 @@
+<?php
+//srting()
+echo strlen("hello world!");
+
+//strpos()
+echo strpos("hello world!","world!");
+
+//str_replace()
+echo"<br>";
+echo str_replace("world!","universe","hello world!");
+
+//substar()
+echo"<br>";
+echo substr("hello world!",0,5);
+
+//strtolower()
+echo"<br>";
+echo strtolower("hello world!");
+
+//strtoupper()
+echo"<br>";
+echo strtoupper("hello world!");
+
+//ucfirst()
+echo"<br>";
+echo ucfirst("hello world!");
+
+//srtrrev()
+echo"<br>";
+echo strrev("hello world");
+
+//trim()
+echo"<br>";
+echo trim("hello world!");
+
+?>
